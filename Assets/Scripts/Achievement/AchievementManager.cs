9d502b6fd16c9b5cb4197b297907d8b5eb27be31
jaehyeonB <didska5518@g.shingu.ac.kr>
2025-11-03 15:36:10 +0900

    [Feat] 10강 미완성

---
+++ Assets/Scripts/Achievement/AchievementManager.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementManager : MonoBehaviour
+{
+    public static AchievementManager instance;
+
+    [Header("Achievement Settings")]
+    public List<AchievementDATA> allAchievements = new List<AchievementDATA>();
+
+    [Header("UI References")]
+    public GameObject achievementPopupPrefab;
+    public Transform popupParent;
+    public GameObject achievementPanel;
+    public Transform achievementListContent;
+    public GameObject achievementSlotPrefab;
+
+    private Dictionary<AchievementType, int> progressData = new Dictionary<AchievementType, int>();
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public float GetProgress(AchievementDATA achievement)
+    {
+        if (achievement.isUnlocked) return 1f;
+        int current = progressData.ContainsKey(achievement.achievementType) ? progressData[achievement.achievementType] : 0;
+        return Mathf.Min((float)current / achievement.requiredAmount, 1f);
+    }
+
+    public void UpdateAchievementUI()
+    {
+        if (achievementListContent == null || achievementSlotPrefab == null)
+            return;
+
+        //기존 슬롯 제거
+        foreach(Transform child in achievementListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach(AchievementDATA achievement in allAchievements)
+        {
+            GameObject slot = Instantiate(achievementSlotPrefab, achievementListContent);
+            AchievementSlot slotScript = slot.GetComponent<AchievementSlot>();
+            if(slotScript != null)
+            {
+                slotScript.SetAchievement(achievement, GetProgress(achievement));
+            }
+        }
+    }
+
+    void SaveAchievements()
+    {
+        foreach (var kvp in progressData)
+        {
+            PlayerPrefs.SetInt("Achievement_" + kvp.Key, kvp.Value);
+        }
+
+        foreach(AchievementDATA achievement in allAchievements)
+        {
+            PlayerPrefs.SetInt("Unlocked_" + achievement.name, achievement.isUnlocked ? 1 : 0);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+    void LoadAchievements()
+    {
+        foreach (AchievementType type in System.Enum.GetValues(typeof(AchievementType)))
+        {
+            progressData[type] = PlayerPrefs.GetInt("Achievement_" + type, 0);
+        }
+
+        foreach (AchievementDATA achievement in allAchievements)
+        {
+            achievement.isUnlocked = PlayerPrefs.GetInt("Unlocked_" + achievement.name, 0) == 1;
+        }
+    }
+
+    public void ResetAllAchivements()
+    {
+        foreach(AchievementType type in System.Enum.GetValues(typeof(AchievementType)))
+        {
+            progressData[type] = 0;
+            PlayerPrefs.DeleteKey("Achievement_" + type);
+        }
+
+        foreach(AchievementDATA achievement in allAchievements)
+        {
+            achievement.isUnlocked = false;
+            PlayerPrefs.DeleteKey("Unlocked_" + achievement.name);
+        }
+
+        PlayerPrefs.Save();
+        UpdateAchievementUI();
+    }
+
+    void ShowAchievementPopup(AchievementDATA achievement)
+    {
+        if(achievementPopupPrefab != null && popupParent != null)
+        {
+            GameObject popup = Instantiate(achievementPopupPrefab, popupParent);
+        }
+    }
+}
